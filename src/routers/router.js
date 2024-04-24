3c813b6ba1323aa163f40e10ed1ae6724ce2e16f
Xen0mrefne <x3n0g3n1c@gmail.com>
2024-04-23 23:56:24 -0300

    deleted unnecesary files

--- src/routers/router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const CalendarController = require("../controllers/calendarController");
-
-const router = express.Router();
-
-router.get('/reminders/:id', CalendarController.getReminderById)
-router.post('/reminders', CalendarController.addReminder)
-
-module.exports = router
--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,5 @@
 const express = require("express")
 const cors = require("cors")
-const router = require("../routers/router")
 const authRouter = require("../routers/auth")
 
 const server = express()
@@ -8,6 +7,5 @@ const server = express()
 server.use(cors({origin: "http://localhost:4200"}))
 server.use(express.json())
 server.use('/auth', authRouter)
-server.use('/api', router)
 
 module.exports = server
